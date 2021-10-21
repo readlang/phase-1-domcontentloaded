59b2e3f79d5d53b10160e2397e88fbbaeb91cf91
Read Langworthy <rlangworthy@gmail.com>
2021-10-21 16:22:45 -0700

    commit tree c2cdb6e4271ef48d1a98b7598e21d9a00f976913

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    element = document.querySelector("#text")
+    element.textContent = "This is really cool!"
+    console.log("The Dom has loaded.");
+})
+
